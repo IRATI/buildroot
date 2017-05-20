593fb3c3690ff85b4967618cd2654eaf068a83b9
Vincenzo Maffione <v.maffione@gmail.com>
2017-05-20 14:21:53 +0200

    irati: align to new build system

--- package/fakelinuxdep/fakelinuxdep.mk
+++ package/fakelinuxdep/fakelinuxdep.mk
@@ -18,23 +18,22 @@ FAKELINUXDEP_INSTALL_TARGET = YES
 FAKELINUXDEP_DEPENDENCIES =
 FAKELINUXDEP_LICENSE = LGPL
 
-define FAKELINUXDEP_CONFIGURE_CMDS
-	echo "fakelinuxdep: configuring"
+FAKELINUXDEP_MODULE_SUBDIRS = kernel
+# Set options that are passed through kernel/configure when building manually.
+FAKELINUXDEP_MODULE_MAKE_OPTS = REGRESSION_TESTS=n HAVE_VMPI=n TCP_UDP_BUFFER_SIZE=1500
+
+# Invoke the ./configure script to generate kernel Makefile
+define FAKELINUXDEP_DO_CONFIGURE
+	echo "fakelinuxdep: patching"
 	for p in $(FAKELINUXDEP_PKGDIR)/*.patch.manual ; do \
 		 patch -d $(@D) -p1 < $$p ; \
 	done
+	echo "fakelinuxdep: Running kernel configure ..."
+	cd $(@D)/kernel && ./configure
+	echo "... done!"
 endef
 
-define FAKELINUXDEP_BUILD_CMDS
-	echo "fakelinuxdep: build"
-endef
+FAKELINUXDEP_PRE_CONFIGURE_HOOKS += FAKELINUXDEP_DO_CONFIGURE
 
-define FAKELINUXDEP_INSTALL_STAGING_CMDS
-	echo "fakelinuxdep: install staging"
-endef
-
-define FAKELINUXDEP_INSTALL_TARGET_CMDS
-	echo "fakelinuxdep: install target"
-endef
-
+$(eval $(kernel-module))
 $(eval $(generic-package))
